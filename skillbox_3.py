63e5dcf97d71563896d16c7d2d938fde74da62fb
IaroslavSorokoumov <123410058+IaroslavSorokoumov@users.noreply.github.com>
2023-01-25 17:05:19 +0300

    added skillbox_3.py for test

---
+++ skillbox_3.py
@@ -0,0 +1,43 @@
+
+##print(6 * 39)
+##print(3 ** 5)
+##print(-8 / -4)
+##print(10 / 2 + 6)
+
+##a, b, c = 5, 4, 2
+##print('Сложение: ', a+b+c)
+##print('Вычитание: ', a-b-c)
+##print('Умножение: ', a*b*c)
+##print('Деление: ', a/b/c)
+##print('Возведение в степень: ', a**b**c)
+
+##name = 'Michael'
+##duration = 120
+##print(f"This intensive drive by {name}, duration {duration} minutes")
+
+##name = 'Michael'
+##duration = 133
+##dur_hours = round(duration / 60, 2)
+###print(f"This intensive drive by {name}, duration {duration} minutes") 
+##print(f"This intensive drive by {name}, duration {dur_hours} hours") 
+
+##name = input('Your name: ')
+##print('Hello ' + name)
+##name2 = input('Second name ')
+##print('Hi ' + name2)
+##print('Hello', name + ' and', name2 + '!')
+##print('Hello ' + name + ' and ' + name2 + '!')
+
+##a, b, c = 8, 5, 3
+##
+##k = ((a / (b + c)) - a) / c
+##print(k)
+
+##bus = 5
+##metro = 3
+##result = ((6 + (10 - bus) ** 2) / (metro + 1)) + 132 / (2 + bus)
+##print(result)
+
+a = (-3 + (8**2 - 12) * 4**3**2) / (2 * 18)
+print(a)
+print()
